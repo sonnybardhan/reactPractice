9283e747bc08fd0af2e41902ecc2bd131574167a
sonnybardhan <sonnybardhan@github.io>
2020-06-24 00:35:29 +0530

    basic boilerplate

--- src/App.js
+++ src/App.js
@@ -1,52 +1,9 @@
 import React from 'react';
 import './App.css';
 
-// import Heads from './Heads';
-// import Tails from './Tails';
-
-import Coin from './Coin';
-
 class App extends React.Component {
-	state = {
-		totalFlips: 0,
-		heads: 0,
-		tails: 0,
-		src: null,
-		alt: null
-	};
-
-	handleFlip = () => {
-		const random = (Math.random() * 2) | 0;
-		if (random === 0) {
-			this.setState({ src: 'https://tinyurl.com/react-coin-heads-jpg', alt: 'heads' });
-			this.setState((prev) => {
-				return { heads: prev.heads + 1 };
-			});
-		} else {
-			this.setState({ src: 'https://tinyurl.com/react-coin-tails-jpg', alt: 'tails' });
-			this.setState((prev) => {
-				return { tails: prev.tails + 1 };
-			});
-		}
-		this.setState((prev) => {
-			return { totalFlips: prev.totalFlips + 1 };
-		});
-	};
-
 	render() {
-		return (
-			<div className="App">
-				<h1>Let's flip a coin</h1>
-				<div className="">
-					<Coin src={this.state.src} alt={this.state.alt} />
-				</div>
-				<p>
-					Out of {this.state.totalFlips} flips, there have been {this.state.heads} heads and{' '}
-					{this.state.tails} tails.
-				</p>
-				<button onClick={this.handleFlip}>Flip</button>
-			</div>
-		);
+		return <div className="App">hello world</div>;
 	}
 }
 
--- src/Coin.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Coin({ src, alt }) {
-	return <img src={src} alt={alt} style={{ width: '150px', height: '150px' }} />;
-}

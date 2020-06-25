f1899adba51a36c0bed48e69a702b3be5194cf68
sonnybardhan <sonnybardhan@github.io>
2020-06-26 02:39:27 +0530

    better input

--- src/Box.js
+++ src/Box.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 export default function Box({ height, width, color, removeBox, id }) {
 	const style = {
-		height: height + 'px',
-		width: width + 'px',
-		backgroundColor: color
+		height: (height || 50) + 'px',
+		width: (width || 100) + 'px',
+		backgroundColor: color || 'green'
 	};
+	// const style = {
+	// 	height: height ? height + 'px' : '100px',
+	// 	width: width ? width + 'px' : '100px',
+	// 	backgroundColor: color ? color : 'black'
+	// };
 
 	function handleRemove() {
 		removeBox(id);

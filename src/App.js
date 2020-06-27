829e94236dd40297369dbc38702493dc66729b84
sonnybardhan <sonnybardhan@github.io>
2020-06-27 20:53:53 +0530

    my hacky router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import MyRouter from './MyRouter';
 import './App.css';
 
 function App() {
-	return <div className="App">hello world</div>;
+	return (
+		<div className="App">
+			<MyRouter />
+		</div>
+	);
 }
 
 export default App;
